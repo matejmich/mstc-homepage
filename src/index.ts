3039258c09cd072b9592bc0b2516eb3802c7bb48
matejmich <matejmich@seznam.cz>
2024-05-29 19:30:49 +0200

    responsive design and dropdown menu missing

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,9 @@ import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs
 //
 const swiper = new Swiper('.swiper', {
   initialSlide: 1,
-    speed: 600,
+    speed: 900,
     parallax: true,
-    slidesPerView: 1.2,
+    slidesPerView: 1,
     a11y: {
         prevSlideMessage: 'Previous slide',
         nextSlideMessage: 'Next slide',
