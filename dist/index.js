ab12db6a54d161687145b27569053a8654db0b8b
matejmich <matejmich@seznam.cz>
2024-05-29 16:37:51 +0200

    slider and dropdown missing

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,12 @@
 // import Swiper JS
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs';
 // import Swiper styles
+//
 const swiper = new Swiper('.swiper', {
+    initialSlide: 1,
+    speed: 600,
+    parallax: true,
+    slidesPerView: 1.2,
     a11y: {
         prevSlideMessage: 'Previous slide',
         nextSlideMessage: 'Next slide',
@@ -10,6 +15,11 @@ const swiper = new Swiper('.swiper', {
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
+        renderBullet: (index, className) => {
+            const text = index === 0 ? 'kámen pro firmu' : 'kámen domů';
+            const customClassName = index === 0 ? 'first-slide' : 'second-slide';
+            return `<span class="${className} ${customClassName}">${text}</span>`;
+        },
     },
     navigation: {
         nextEl: '.swiper-button-next',
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 // import Swiper JS
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
 // import Swiper styles
+
+//
 const swiper = new Swiper('.swiper', {
+  initialSlide: 1,
+    speed: 600,
+    parallax: true,
+    slidesPerView: 1.2,
     a11y: {
         prevSlideMessage: 'Previous slide',
         nextSlideMessage: 'Next slide',
@@ -10,6 +16,11 @@ const swiper = new Swiper('.swiper', {
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
+      renderBullet: (index: number, className: string) => {
+        const text = index === 0 ? 'kámen pro firmu' : 'kámen domů';
+        const customClassName = index === 0 ? 'first-slide' : 'second-slide';
+        return `<span class="${className} ${customClassName}">${text}</span>`;
+      },
     },
     navigation: {
       nextEl: '.swiper-button-next',
